d61a006b5138838795d205fe64d973d6c084f0b7
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-12-10 12:47:16 +0100

    Legger til config i test-scope, fjerner unødvendige filer

--- src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
+++ src/main/kotlin/no/nav/syfo/selftest/SelftestController.kt
@@ -9,16 +9,16 @@ const val APPLICATION_LIVENESS = "Application is alive!"
 const val APPLICATION_READY = "Application is ready!"
 
 @RestController
-@RequestMapping(value = ["/internal"])
+@RequestMapping(value = "/internal")
 class SelftestController {
 
     val isAlive: String
         @ResponseBody
-        @RequestMapping(value = ["/isAlive"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        @RequestMapping(value = "/isAlive", produces = [MediaType.TEXT_PLAIN_VALUE])
         get() = APPLICATION_LIVENESS
 
     val isReady: String
         @ResponseBody
-        @RequestMapping(value = ["/isReady"], produces = [MediaType.TEXT_PLAIN_VALUE])
+        @RequestMapping(value = "/isReady", produces = [MediaType.TEXT_PLAIN_VALUE])
         get() = APPLICATION_READY
 }
