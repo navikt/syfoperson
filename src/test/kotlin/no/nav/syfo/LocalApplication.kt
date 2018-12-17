2198b13e1169ae6fe287943cc810c9eda651be57
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-12-17 12:08:58 +0100

    Skrur paa Spring caching, aktiverer Redis i NAIS

--- src/test/kotlin/no/nav/syfo/LocalApplication.kt
+++ src/test/kotlin/no/nav/syfo/LocalApplication.kt
@@ -3,9 +3,11 @@ package no.nav.syfo
 import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @EnableOIDCTokenValidation(ignore=["org.springframework"])
 @SpringBootApplication
+@EnableCaching
 class LocalApplication
     fun main(args: Array<String>) {
         runApplication<LocalApplication>(*args)
