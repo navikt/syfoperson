4f5bc5a0d220cddd703f1dc22cbfd1c54fabcd83
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-12-17 12:08:58 +0100

    Skrur paa Spring caching, fjerner unodvendig kode

--- src/main/kotlin/no/nav/syfo/Application.kt
+++ src/main/kotlin/no/nav/syfo/Application.kt
@@ -1,21 +1,15 @@
 package no.nav.syfo
 
 import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-
-const val CALL_ID = "callId"
+import org.springframework.cache.annotation.EnableCaching
 
 @EnableOIDCTokenValidation(ignore = ["org.springframework"])
 @SpringBootApplication
+@EnableCaching
 class Application
 
-fun main(args: Array<String>) {
-    runApplication<Application>(*args)
-}
-
-inline fun <reified T> T.log(): Logger {
-    return LoggerFactory.getLogger(T::class.java)
-}
+    fun main(args: Array<String>) {
+        runApplication<Application>(*args)
+    }
