153a2145de718a52dc364e477495a47728870304
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2018-12-19 09:18:06 +0100

    Setter opp caching med Redis

--- src/main/kotlin/no/nav/syfo/Application.kt
+++ src/main/kotlin/no/nav/syfo/Application.kt
@@ -3,11 +3,9 @@ package no.nav.syfo
 import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
 @EnableOIDCTokenValidation(ignore = ["org.springframework"])
 @SpringBootApplication
-@EnableCaching
 class Application
 
     fun main(args: Array<String>) {
---
+++ src/main/kotlin/no/nav/syfo/config/caching/CacheConfig.kt
@@ -0,0 +1,43 @@
+package no.nav.syfo.config.caching
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisNode
+import org.springframework.data.redis.connection.RedisSentinelConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import java.time.Duration
+
+@Configuration
+@EnableCaching
+class CacheConfig
+
+    private val REDIS_SENTINEL_PORT : Int = System.getProperty("redisSentinelPort").toInt()
+    private val REDIS_SENTINEL_HOST : String = "rfs-" + System.getProperty("redisSentinelHost")
+    private val REDIS_MASTER : String = System.getProperty("redisMaster")
+    private val CACHE_NAMES : Set<String> = arrayOf("redisCache").toSet()
+    private val ENTRY_TTL : Duration = Duration.ofMillis(60000)
+
+    @Bean
+    fun cacheManager(lettuceConnectionFactory: LettuceConnectionFactory) : RedisCacheManager {
+        val redisCacheConfig : RedisCacheConfiguration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .entryTtl(ENTRY_TTL)
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(lettuceConnectionFactory)
+                .cacheDefaults(redisCacheConfig)
+                .initialCacheNames(CACHE_NAMES)
+                .build()
+    }
+
+    @Bean
+    fun lettuceConnectionFactory() : LettuceConnectionFactory {
+        return LettuceConnectionFactory(RedisSentinelConfiguration()
+                                            .master(REDIS_MASTER)
+                                            .sentinel(RedisNode(REDIS_SENTINEL_HOST, REDIS_SENTINEL_PORT)))
+    }
+
+
+
