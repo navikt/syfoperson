a2b94965d8adf357fee6e98c51c0c63f48b7dc0c
Tor-Christian Tangenes <Tor-Christian.Tangenes@nav.no>
2019-01-03 14:40:43 +0100

    Migration script som setter opp tabell for aa lagre knytning mellom veileder og bruker Aktiverer flyway og helsesjekk paa database Rydder litt i koden
    SYFOUTV-2598

--- src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
@@ -1,6 +1,8 @@
 package no.nav.syfo.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
+@EnableTransactionManagement
 class ApplicationConfig
