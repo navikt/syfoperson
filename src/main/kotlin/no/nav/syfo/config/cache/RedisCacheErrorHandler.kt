7bd804207d9da8a4c9b3b7d5842ca728cf6029db
Tor-Christian Tangenes <torchrct@gmail.com>
2019-08-26 10:36:51 +0200

    Switch from sentinel to standalone-mode for Redis cache

---
+++ src/main/kotlin/no/nav/syfo/config/cache/RedisCacheErrorHandler.kt
@@ -0,0 +1,35 @@
+package no.nav.syfo.config.cache
+
+import no.nav.syfo.service.PersonService
+import org.slf4j.LoggerFactory
+import org.springframework.cache.Cache
+import org.springframework.cache.interceptor.CacheErrorHandler
+import org.springframework.lang.NonNull
+import org.springframework.lang.Nullable
+import java.lang.RuntimeException
+
+class RedisCacheErrorHandler : CacheErrorHandler {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(PersonService::class.java)
+    }
+
+    override fun handleCacheGetError(@NonNull exception: RuntimeException, @NonNull cache: Cache, @NonNull o: Any) {
+        LOG.warn("Feil ved Cache Get operasjon. CacheNavn=${cache.name}, feilklasse=${exception.javaClass.simpleName}, feilmelding=${exception.message}")
+    }
+
+    override fun handleCachePutError(@NonNull exception: RuntimeException, @NonNull cache: Cache,
+                                     @NonNull key: Any, @Nullable value: Any?) {
+        LOG.warn("Feil ved Cache Put operasjon. CacheNavn=${cache.name}, feilklasse=${exception.javaClass.simpleName}, feilmelding=${exception.message}")
+    }
+
+    override fun handleCacheEvictError(@NonNull exception: RuntimeException, @NonNull cache: Cache,
+                                       @NonNull key: Any) {
+        LOG.warn("Feil ved Cache Evict operasjon. CacheNavn=${cache.name}, feilklasse=${exception.javaClass.simpleName}, feilmelding=${exception.message}")
+    }
+
+    override fun handleCacheClearError(@NonNull exception: RuntimeException, @NonNull cache: Cache) {
+        LOG.warn("Feil ved Cache Clear operasjon. CacheNavn=${cache.name}, feilklasse=${exception.javaClass.simpleName}, feilmelding=${exception.message}")
+    }
+
+}
