4176643e5d10a7d84c871ebbe21d264a56f5b9c5
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-18 09:22:45 +0100

    Remove duplicate @EnableCaching

--- src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/ApplicationConfig.kt
@@ -1,13 +1,11 @@
 package no.nav.syfo.config
 
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestTemplate
 
 @Configuration
-@EnableCaching
 class ApplicationConfig {
     @Bean
     fun restTemplate(vararg interceptors: ClientHttpRequestInterceptor): RestTemplate {
