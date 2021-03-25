b0835351766776ed853990ed8431b351b106ed43
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-03-25 09:41:36 +0100

    Add regex for exactly 11 digits to Fnr

--- src/main/kotlin/no/nav/syfo/person/api/domain/Fnr.kt
+++ src/main/kotlin/no/nav/syfo/person/api/domain/Fnr.kt
@@ -1,3 +1,11 @@
 package no.nav.syfo.person.api.domain
 
-data class Fnr(val fnr: String = "")
+data class Fnr(val fnr: String = "") {
+    private val elevenDigits = Regex("^\\d{11}\$")
+
+    init {
+        if (!elevenDigits.matches(fnr)) {
+            throw IllegalArgumentException("Value is not a valid PersonIdentNumber")
+        }
+    }
+}
