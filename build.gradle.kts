26b6241db298d89e171807a155f93ffc4e0ee62d
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-06-04 15:56:52 +0200

    Bump Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ val javaxActivationVersion = "1.2.0"
 val kotlinJacksonVersion = "2.11.2"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
+    kotlin("jvm") version "1.4.32"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.0"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
     id("org.springframework.boot") version "2.3.7.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
