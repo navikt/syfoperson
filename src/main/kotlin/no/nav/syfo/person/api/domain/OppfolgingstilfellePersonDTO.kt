dbea3ab4f08ccbf41d82cdd811bc46f96608ea11
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-09-30 20:30:18 +0200

    Remove unused functionality for Oppfolgingstilfelle

--- src/main/kotlin/no/nav/syfo/person/api/domain/OppfolgingstilfellePersonDTO.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.syfo.person.api.domain
-
-import java.time.LocalDate
-
-data class OppfolgingstilfellePersonDTO(
-    val fom: LocalDate? = null,
-    val tom: LocalDate? = null,
-)
--- src/main/kotlin/no/nav/syfo/person/api/v2/PersonControllerV2.kt
+++ src/main/kotlin/no/nav/syfo/person/api/v2/PersonControllerV2.kt
@@ -6,12 +6,9 @@ import no.nav.syfo.consumer.dkif.DigitalKontaktinfoBolk
 import no.nav.syfo.consumer.dkif.DkifConsumer
 import no.nav.syfo.consumer.pdl.*
 import no.nav.syfo.consumer.skjermedepersoner.SkjermedePersonerPipConsumer
-import no.nav.syfo.consumer.syketilfelle.domain.toOppfolgingstilfellePersonDTO
 import no.nav.syfo.consumer.veiledertilgang.VeilederTilgangConsumer
-import no.nav.syfo.person.Oppfolgingstilfelle.OppfolgingstilfelleService
 import no.nav.syfo.person.api.domain.*
 import no.nav.syfo.person.skjermingskode.SkjermingskodeService
-import no.nav.syfo.util.getOrCreateCallId
 import no.nav.syfo.util.getPersonIdent
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.util.MultiValueMap
@@ -23,7 +20,6 @@ import javax.inject.Inject
 @ProtectedWithClaims(issuer = VEILEDER_AZURE_V2)
 class PersonControllerV2 @Inject constructor(
     val dkifConsumer: DkifConsumer,
-    val oppfolgingstilfelleService: OppfolgingstilfelleService,
     val pdlConsumer: PdlConsumer,
     val skjermingskodeService: SkjermingskodeService,
     val skjermedePersonerPipConsumer: SkjermedePersonerPipConsumer,
@@ -121,25 +117,5 @@ class PersonControllerV2 @Inject constructor(
         veilederTilgangConsumer.throwExceptionIfDeniedAccessAzureOBO(requestedPersonIdent)
 
         return dkifConsumer.digitalKontaktinfoBolk(requestedPersonIdent)
-    }
-
-    @ResponseBody
-    @GetMapping(value = ["/oppfolgingstilfelle"], produces = [APPLICATION_JSON_VALUE])
-    fun getOppfolgingstilfelle(
-        @RequestHeader headers: MultiValueMap<String, String>
-    ): OppfolgingstilfellePersonDTO {
-        val callId = getOrCreateCallId(headers)
-
-        val requestedPersonIdent = headers.getPersonIdent()?.let { personIdent ->
-            Fnr(personIdent)
-        } ?: throw IllegalArgumentException("Did not find a PersonIdent in request headers")
-
-        veilederTilgangConsumer.throwExceptionIfDeniedAccessAzureOBO(requestedPersonIdent)
-
-        return oppfolgingstilfelleService.oppfolgingstilfelle(
-            personIdent = requestedPersonIdent,
-            callId = callId,
-        )?.toOppfolgingstilfellePersonDTO()
-            ?: throw RuntimeException("PersonIdent has no Oppfolgingstilfelle")
     }
 }
