5b1d61ce782de9ba9704ef6974666f945d8a9cc3
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-10-07 15:30:57 +0200

    Move application to GCP (#84)

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,4 @@ FROM navikt/java:11-appdynamics
 COPY build/libs/app.jar app.jar
 
 ENV JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom \
-               -Dspring.profiles.active=remote \
-               -Dhttps.proxyHost=webproxy-nais.nav.no \
-               -Dhttps.proxyPort=8088 \
-               -Dhttp.nonProxyHosts=*.adeo.no|*.preprod.local"
+               -Dspring.profiles.active=remote"
--- src/main/kotlin/no/nav/syfo/consumer/skjermedepersoner/SkjermedePersonerPipConsumer.kt
+++ src/main/kotlin/no/nav/syfo/consumer/skjermedepersoner/SkjermedePersonerPipConsumer.kt
@@ -1,9 +1,14 @@
 package no.nav.syfo.consumer.skjermedepersoner
 
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.syfo.api.auth.OIDCIssuer
+import no.nav.syfo.api.auth.OIDCUtil
 import no.nav.syfo.cache.CacheConfig.Companion.EGNEANSATTBYFNR
+import no.nav.syfo.consumer.azuread.v2.AzureAdV2TokenConsumer
 import no.nav.syfo.metric.Metric
 import no.nav.syfo.util.*
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.*
 import org.springframework.stereotype.Service
@@ -13,17 +18,32 @@ import javax.inject.Inject
 
 @Service
 class SkjermedePersonerPipConsumer @Inject constructor(
+    @Value("\${skjermedepersonerpip.client.id}") private val skjermedePersonerPipClientId: String,
+    @Value("\${skjermedepersonerpip.url}") private val skjermedePersonerPipBaseUrl: String,
+    private val azureAdV2TokenConsumer: AzureAdV2TokenConsumer,
+    private val contextHolder: TokenValidationContextHolder,
     private val metric: Metric,
     private val restTemplate: RestTemplate
 ) {
     @Cacheable(cacheNames = [EGNEANSATTBYFNR], key = "#personIdent", condition = "#personIdent != null")
     fun erSkjermet(personIdent: String): Boolean {
+        val token = OIDCUtil.tokenFraOIDC(contextHolder, OIDCIssuer.VEILEDER_AZURE_V2)
+        val veilederId = OIDCUtil.getNAVIdentFraOIDC(contextHolder)
+            ?: throw RuntimeException("Missing veilederId in OIDC-context")
+        val azp = OIDCUtil.getAZPFraOIDC(contextHolder)
+            ?: throw RuntimeException("Missing azp in OIDC-context")
+        val oboToken = azureAdV2TokenConsumer.getOnBehalfOfToken(
+            scopeClientId = skjermedePersonerPipClientId,
+            token = token,
+            veilederId = veilederId,
+            azp = azp,
+        )
         try {
             val response = restTemplate.exchange(
                 getSkjermedePersonerPipUrl(personIdent),
                 HttpMethod.GET,
-                entity(),
-                Boolean::class.java
+                entity(token = oboToken),
+                Boolean::class.java,
             )
             val skjermedePersonerResponse = response.body!!
             metric.countEvent("call_skjermede_person_pip_success")
@@ -37,12 +57,13 @@ class SkjermedePersonerPipConsumer @Inject constructor(
     }
 
     private fun getSkjermedePersonerPipUrl(personIdent: String): String {
-        return "http://skjermede-personer-pip.nom.svc.nais.local/skjermet?personident=$personIdent"
+        return "$skjermedePersonerPipBaseUrl/skjermet?personident=$personIdent"
     }
 
-    private fun entity(): HttpEntity<String> {
+    private fun entity(token: String): HttpEntity<String> {
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
+        headers[HttpHeaders.AUTHORIZATION] = bearerHeader(token)
         headers[NAV_CONSUMER_ID_HEADER] = APP_CONSUMER_ID
         headers[NAV_CALL_ID_HEADER] = createCallId()
         return HttpEntity(headers)
