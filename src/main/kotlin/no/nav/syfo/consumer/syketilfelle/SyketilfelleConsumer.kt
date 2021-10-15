7b049a59ac4d6c586e19b96ed042d748953463bb
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-10-15 11:04:30 +0200

    Replace endpoint to get Oppfolgingstilfelle for Person and Virksomhet in Modiasyforest (#88)

--- src/main/kotlin/no/nav/syfo/consumer/syketilfelle/SyketilfelleConsumer.kt
+++ src/main/kotlin/no/nav/syfo/consumer/syketilfelle/SyketilfelleConsumer.kt
@@ -4,6 +4,7 @@ import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.syfo.api.auth.OIDCIssuer
 import no.nav.syfo.api.auth.OIDCUtil
 import no.nav.syfo.consumer.azuread.v2.AzureAdV2TokenConsumer
+import no.nav.syfo.domain.Virksomhetsnummer
 import no.nav.syfo.metric.Metric
 import no.nav.syfo.person.api.domain.AktorId
 import no.nav.syfo.util.*
@@ -24,6 +25,56 @@ class SyketilfelleConsumer @Inject constructor(
     private val metric: Metric,
     private val restTemplate: RestTemplate,
 ) {
+    fun getOppfolgingstilfellePersonArbeidsgiver(
+        aktorId: AktorId,
+        callId: String,
+        virksomhetsnummer: Virksomhetsnummer,
+    ): KOppfolgingstilfelle? {
+        val token = OIDCUtil.tokenFraOIDC(contextHolder, OIDCIssuer.VEILEDER_AZURE_V2)
+
+        val veilederId = OIDCUtil.getNAVIdentFraOIDC(contextHolder)
+            ?: throw RuntimeException("Missing veilederId in OIDC-context")
+        val azp = OIDCUtil.getAZPFraOIDC(contextHolder)
+            ?: throw RuntimeException("Missing azp in OIDC-context")
+        val oboToken = azureAdV2TokenConsumer.getOnBehalfOfToken(
+            scopeClientId = isproxyClientId,
+            token = token,
+            veilederId = veilederId,
+            azp = azp,
+        )
+
+        try {
+            val url = getSyfosyketilfelleUrl(
+                aktorId = aktorId,
+                virksomhetsnummer = virksomhetsnummer,
+            )
+            val response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity(token = oboToken),
+                KOppfolgingstilfelle::class.java
+            )
+            if (response.statusCode == HttpStatus.NO_CONTENT) {
+                LOG.info("Syketilfelle returned HTTP-${response.statusCodeValue}: No Oppfolgingstilfelle was found for AktorId")
+                return null
+            }
+            val responseBody: KOppfolgingstilfelle = response.body!!
+            metric.countEvent(CALL_SYFOSYKETILFELLE_PERSON_NO_ARBEIDSGIVER_FAIL)
+            return responseBody
+        } catch (e: RestClientResponseException) {
+            LOG.error("Request to get Oppfolgingstilfelle for Person and Arbeidsgiver from Isproxy-Syfosyketilfelle failed with status ${e.rawStatusCode} and message: ${e.responseBodyAsString}")
+            metric.countEvent(CALL_SYFOSYKETILFELLE_PERSON_NO_ARBEIDSGIVER_SUCCESS)
+            throw e
+        }
+    }
+
+    private fun getSyfosyketilfelleUrl(
+        aktorId: AktorId,
+        virksomhetsnummer: Virksomhetsnummer,
+    ): String {
+        return "$isproxyBaseUrl$ISPROXY_SYFOSYKETILFELLE_OPPFOLGINGSTILFELLE_PERSON_PATH/${aktorId.value}/${virksomhetsnummer.value}"
+    }
+
     fun getOppfolgingstilfellePerson(
         aktorId: AktorId,
         callId: String,
