8d804c1a519e39de41a19d8e546498eb81a06231
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-12-20 10:46:54 +0100

    Bump logback to latest version 1.2.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ object Versions {
     const val jedis = "3.7.1"
     const val ktor = "1.6.7"
     const val kluent = "1.68"
-    const val logback = "1.2.8"
+    const val logback = "1.2.9"
     const val logstashEncoder = "7.0.1"
     const val mockk = "1.12.1"
     const val nimbusJoseJwt = "9.15.2"
