c9d8a9c268e48bd6ffd7a1ca15327057fa286a4c
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-03-11 14:42:25 +0100

    Bump dependency versions to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,13 @@ object Versions {
     const val jedis = "4.1.1"
     const val ktor = "1.6.7"
     const val kluent = "1.68"
-    const val logback = "1.2.10"
+    const val logback = "1.2.11"
     const val logstashEncoder = "7.0.1"
-    const val mockk = "1.12.2"
-    const val nimbusJoseJwt = "9.18"
-    const val micrometerRegistry = "1.8.2"
+    const val mockk = "1.12.3"
+    const val nimbusJoseJwt = "9.21"
+    const val micrometerRegistry = "1.8.3"
     const val redisEmbedded = "0.7.3"
-    const val spek = "2.0.17"
+    const val spek = "2.0.18"
 }
 
 plugins {
