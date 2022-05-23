7b85cd5a3f78b7942910721a8fc1414cca46d6f6
geir-waagboe <geir.arne.waagbo@nav.no>
2022-05-23 12:57:26 +0200

    Retry on client exceptions

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.client
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import no.nav.syfo.util.configure
@@ -15,6 +16,12 @@ fun httpClientDefault() = HttpClient(CIO) {
             configure()
         }
     }
+    install(HttpRequestRetry) {
+        retryOnExceptionIf(2) { _, cause ->
+            cause !is ClientRequestException
+        }
+        constantDelay(500L)
+    }
     expectSuccess = true
 }
 
@@ -23,6 +30,12 @@ val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
         jackson {
             configure()
         }
+    }
+    install(HttpRequestRetry) {
+        retryOnExceptionIf(2) { _, cause ->
+            cause !is ClientRequestException
+        }
+        constantDelay(500L)
     }
     expectSuccess = true
     engine {
