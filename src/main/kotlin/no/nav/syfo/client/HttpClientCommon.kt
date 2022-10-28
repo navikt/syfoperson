e656aa97ae95a38fc6001caf7e1e0a0b7f9f5c2f
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-10-28 12:56:13 +0200

    Test using Apache client instead of CIO (#118)

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -1,8 +1,8 @@
 package no.nav.syfo.client
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.engine.apache.*
-import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
@@ -10,11 +10,9 @@ import no.nav.syfo.util.configure
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
 
-fun httpClientDefault() = HttpClient(CIO) {
+val commonConfig: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {
     install(ContentNegotiation) {
-        jackson {
-            configure()
-        }
+        jackson { configure() }
     }
     install(HttpRequestRetry) {
         retryOnExceptionIf(2) { _, cause ->
@@ -26,23 +24,14 @@ fun httpClientDefault() = HttpClient(CIO) {
 }
 
 val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
-    install(ContentNegotiation) {
-        jackson {
-            configure()
-        }
-    }
-    install(HttpRequestRetry) {
-        retryOnExceptionIf(2) { _, cause ->
-            cause !is ClientRequestException
-        }
-        constantDelay(500L)
-    }
-    expectSuccess = true
+    this.commonConfig()
     engine {
         customizeClient {
             setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
         }
     }
 }
+
+fun httpClientDefault() = HttpClient(Apache, commonConfig)
 
 fun httpClientProxy() = HttpClient(Apache, proxyConfig)
