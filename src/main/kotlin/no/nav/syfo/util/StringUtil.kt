f7e9cf43f4635561155edff5df496e5eb067c459
Eirik Dahlen <dahleneirik@gmail.com>
2022-12-12 10:20:43 +0100

    Add name to response object in API
    Frontenden syfooversikt forventer allerede dette feltet, og bruker det også i koden som en backup dersom syfooversiktsrv ikke har navn. Ettersom vi uansett gjør kallet mot PDL og har navnet, så kan vi like godt sende det med.
    
    Fikser også full-navn logikken på samme måte som i syfooversiktsrv.

--- src/main/kotlin/no/nav/syfo/util/StringUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/StringUtil.kt
@@ -3,6 +3,16 @@ package no.nav.syfo.util
 import java.util.*
 
 fun String.lowerCapitalize() =
+    this.split(" ").joinToString(" ") { name ->
+        val nameWithDash = name.split("-")
+        if (nameWithDash.size > 1) {
+            nameWithDash.joinToString("-") { it.capitalizeName() }
+        } else {
+            name.capitalizeName()
+        }
+    }
+
+private fun String.capitalizeName() =
     this.lowercase(Locale.getDefault()).replaceFirstChar {
         if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
     }
