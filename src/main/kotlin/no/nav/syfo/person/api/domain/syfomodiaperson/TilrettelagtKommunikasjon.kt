c17400edab6a3173cfa44a706dd10744d3de4754
Solgård <vetle.busol@outlook.com>
2023-07-03 15:31:50 +0200

    Added sealed class for TilrettelagtKommunikasjon

--- src/main/kotlin/no/nav/syfo/person/api/domain/syfomodiaperson/TilrettelagtKommunikasjon.kt
+++ src/main/kotlin/no/nav/syfo/person/api/domain/syfomodiaperson/TilrettelagtKommunikasjon.kt
@@ -1,10 +1,15 @@
 package no.nav.syfo.person.api.domain.syfomodiaperson
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.WRAPPER_OBJECT, property = "type")
 sealed class TilrettelagtKommunikasjon {
     abstract val sprak: String
+
+    @JsonTypeName(value = "talesprakTolk")
     data class TalesprakTolk(override val sprak: String) : TilrettelagtKommunikasjon()
+
+    @JsonTypeName(value = "tegnsprakTolk")
     data class TegnsprakTolk(override val sprak: String) : TilrettelagtKommunikasjon()
 }
