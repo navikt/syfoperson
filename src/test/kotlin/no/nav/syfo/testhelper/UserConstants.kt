284326122c847b15c0c231a51679ec3649173074
Anders Rognstad <anders.rognstad@nav.no>
2023-11-14 11:21:26 +0100

    Remove unused code

--- src/test/kotlin/no/nav/syfo/testhelper/UserConstants.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/UserConstants.kt
@@ -4,7 +4,6 @@ import no.nav.syfo.domain.*
 
 object UserConstants {
     val ARBEIDSTAKER_PERSONIDENT = PersonIdentNumber("12345678912")
-    val ARBEIDSTAKER_AKTORID = AktorId("10" + ARBEIDSTAKER_PERSONIDENT.value)
     val ARBEIDSTAKER_ALTERNATIVE_PERSONIDENT = PersonIdentNumber("12345678913")
 
     val ARBEIDSTAKER_VEILEDER_NO_ACCESS = PersonIdentNumber(ARBEIDSTAKER_PERSONIDENT.value.replace("2", "1"))
@@ -19,8 +18,6 @@ object UserConstants {
     const val PERSON_NAME_FIRST = "First"
     const val PERSON_NAME_MIDDLE = "Middle"
     const val PERSON_NAME_LAST = "Last"
-
-    val VIRKSOMHETSNUMMER_DEFAULT = Virksomhetsnummer("912345678")
 
     const val VEILEDER_IDENT = "Z999999"
 }
