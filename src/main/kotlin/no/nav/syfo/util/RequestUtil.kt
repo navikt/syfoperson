0caff4c95d689715083cde6351989c0a77305330
Solgård <vetle.bu.solgard@nav.no>
2024-02-26 10:25:18 +0100

    IS-1477: Add behandlingsnummer header to pdl requests

--- src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/RequestUtil.kt
@@ -12,9 +12,6 @@ const val NAV_PERSONIDENTER_HEADER = "Nav-Personidenter"
 const val NAV_CONSUMER_ID_HEADER = "Nav-Consumer-Id"
 const val NAV_CONSUMER_APP_ID = "syfoperson"
 
-const val TEMA_HEADER = "Tema"
-const val ALLE_TEMA_HEADERVERDI = "GEN"
-
 fun bearerHeader(token: String): String {
     return "Bearer $token"
 }
