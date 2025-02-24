51a7c5b5ad6292b5bb6084c0ab4d25816c34e224
geir-waagboe <geir.arne.waagbo@nav.no>
2025-02-24 15:47:50 +0100

    IS-3096: Ikke tillate utgått personident

--- src/main/kotlin/no/nav/syfo/client/pdl/PdlClientMetric.kt
+++ src/main/kotlin/no/nav/syfo/client/pdl/PdlClientMetric.kt
@@ -10,9 +10,6 @@ const val CALL_PDL_PERSON_BASE = "${CALL_PDL_BASE}_person"
 const val CALL_PDL_PERSON_SUCCESS = "${CALL_PDL_PERSON_BASE}_success_count"
 const val CALL_PDL_PERSON_FAIL = "${CALL_PDL_PERSON_BASE}_fail_count"
 
-const val CALL_PDL_ADRESSEBESKYTTELSE_CACHE_HIT = "${CALL_PDL_PERSON_BASE}_adressebeskyttelse_cache_hit_count"
-const val CALL_PDL_ADRESSEBESKYTTELSE_CACHE_MISS = "${CALL_PDL_PERSON_BASE}_adressebeskyttelse_cache_miss_count"
-
 const val CALL_PDL_HENTPERSON_CACHE_HIT = "${CALL_PDL_PERSON_BASE}_hentperson_cache_hit_count"
 const val CALL_PDL_HENTPERSON_CACHE_MISS = "${CALL_PDL_PERSON_BASE}_hentperson_cache_miss_count"
 
@@ -21,13 +18,6 @@ val COUNT_CALL_PDL_PERSON_SUCCESS: Counter = Counter.builder(CALL_PDL_PERSON_SUC
     .register(METRICS_REGISTRY)
 val COUNT_CALL_PDL_PERSON_FAIL: Counter = Counter.builder(CALL_PDL_PERSON_FAIL)
     .description("Counts the number of failed calls to persondatalosning - person")
-    .register(METRICS_REGISTRY)
-
-val COUNT_CALL_PDL_ADRESSEBESKYTTELSE_CACHE_HIT: Counter = Counter.builder(CALL_PDL_ADRESSEBESKYTTELSE_CACHE_HIT)
-    .description("Counts the number of cache hits for calls to pdl - person adressebeskyttelse")
-    .register(METRICS_REGISTRY)
-val COUNT_CALL_PDL_ADRESSEBESKYTTELSE_CACHE_MISS: Counter = Counter.builder(CALL_PDL_ADRESSEBESKYTTELSE_CACHE_MISS)
-    .description("Counts the number of cache misses for calls to pdl - person adressebeskyttelse")
     .register(METRICS_REGISTRY)
 
 val COUNT_CALL_PDL_HENTPERSON_CACHE_HIT: Counter = Counter.builder(CALL_PDL_HENTPERSON_CACHE_HIT)
